2d597e476084054b4255e4e9bd017fc26df27bae
donatobm <donatobevilacqua18@gmail.com>
2023-10-21 01:32:33 -0400

    style: add simple styles for the text inputs
    Simple layout for how it should be the Login/Register view

--- App.tsx
+++ App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableNativeFeedback, Alert } from 'react-native';
+import { StyleSheet, View, TouchableWithoutFeedback, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <TouchableNativeFeedback
-        onPress={() => Alert.alert("Auxilio")}
-      >
-        <Text>ausilio</Text>
-      </TouchableNativeFeedback>
+    <View style={styles.container}> 
+      <TouchableWithoutFeedback>
+        <TextInput style={styles.input} placeholder='Username' />
+      </TouchableWithoutFeedback>
+      <TouchableWithoutFeedback>
+        <TextInput style={styles.input} placeholder='First name' />
+      </TouchableWithoutFeedback>
+      <TouchableWithoutFeedback>
+        <TextInput style={styles.input} placeholder='Last name' />
+      </TouchableWithoutFeedback>
+      <TouchableWithoutFeedback>
+        <TextInput style={styles.input} placeholder='Password' />
+      </TouchableWithoutFeedback>
     </View>
   );
 }
@@ -16,8 +23,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFFFAA',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  input: {
+    margin: 16,
+    width: 300,
+    height: 50,
+    textAlign: "left",
+    padding: 15,
+    backgroundColor: "green",
+    borderRadius: 5,
+    borderColor: "#FFBB00",
+    borderWidth: 2,
+    color: "#FFF",
+    fontSize: 20
+  },
+  
 });
