1510232b0789f6a116c3a4374532372a2a7ad768
donatobm <donatobevilacqua18@gmail.com>
2023-10-21 05:34:49 -0400

    chore: :wrench: update linter files
    Mano les hablo claro, he hecho tantos cambios, que ya se me hace dificil escribir los commits xD

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,59 @@
 module.exports = {
   env: {
     es2021: true,
-    node: true
+    node: true,
   },
+  parser: '@typescript-eslint/parser',
   extends: [
-    'standard-with-typescript',
-    'plugin:prettier/recommended',
-    'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-native/all'
+    'plugin:react-native/all',
+    'plugin:prettier/recommended',
   ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   plugins: ['@typescript-eslint', 'import', 'prettier'],
   overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
       files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: 'script'
-      }
-    }
+        sourceType: 'script',
+      },
+    },
   ],
   settings: {
+    react: {
+      version: 'detect',
+    },
     'import/resolver': {
       node: {
-        extensions: ['.js', '.ts'],
-        paths: ['./src']
+        extensions: ['.js', '.ts', '.jsx', '.tsx'],
+        paths: ['./src'],
       },
       typescript: {
-        alwaysTryTypes: true
-      }
-    }
+        alwaysTryTypes: true,
+      },
+    },
+  },
+  env: {
+    'react-native/react-native': true,
   },
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json'
+    project: './tsconfig.json',
   },
   rules: {
     'prettier/prettier': 'error',
-    'import/no-unresolved': 'error'
-  }
+    'import/no-unresolved': 'error',
+    'react/prop-types': 'off',
+  },
 };
