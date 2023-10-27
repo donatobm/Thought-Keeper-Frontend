b6050f46c2450c0ea14a4f22a496927fd144f0f7
francescorjm <francescorjm@gmail.com>
2023-10-27 01:47:14 -0400

    fix(ui): :ambulance: fix login screen
    I forgot to implement the login screen before

--- src/screens/PublicRoutes/LoginScreen.tsx
+++ src/screens/PublicRoutes/LoginScreen.tsx
@@ -1,8 +1,46 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useContext } from 'react';
+import { Formik } from 'formik';
+import { View, Button } from 'react-native';
+import { useNavigate } from 'react-router-native';
+
+import AuthContext from '../../context/AuthContext';
+import FormInputValue from '../../components/Inputs/FormInputValue';
+
+const initialValues = {
+  username: '',
+  password: '',
+};
 
 const LoginScreen = () => {
-  return <Text>Login Screen</Text>;
+  const { login } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const loginUser = () => {
+    login('asdsadasds');
+  };
+  const handleRegisterOnPress = () => {
+    navigate('/register');
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={values => console.log(values)}
+    >
+      {({ handleSubmit }) => {
+        return (
+          <View>
+            <FormInputValue name="user_name" placeholder="Username" />
+            <FormInputValue name="password" placeholder="Password" />
+            <Button onPress={loginUser} title="Login" />
+
+            <Button onPress={handleRegisterOnPress} title="Or Register" />
+          </View>
+        );
+      }}
+    </Formik>
+  );
 };
 
 export default LoginScreen;
